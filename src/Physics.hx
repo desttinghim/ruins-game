0b520408f21d2d0a3a913bc37330e61d17407983
Louis Pearson <desttinghimgame@gmail.com>
2015-02-13 13:43:10 -0600

    Physics component movement works.

--- src/Physics.hx
+++ src/Physics.hx
@@ -6,6 +6,7 @@
 
 import luxe.Component;
 import luxe.Vector;
+import luxe.Sprite;
 
 import luxe.collision.Collision;
 import luxe.collision.CollisionData;
@@ -22,35 +23,63 @@ class Physics extends Component
 	var kinetic : Bool = false;
 	var collides : Bool = false;
 
-	public function new( gravity : Bool, kinetic : Bool, collides : false)
+	public function new( kinetic:Bool, collides:Bool, ?gravity:Vector)
 	{
 		// I don't know if I'll use this
-		this.gravity = gravity;
+		super({ name: 'physics' });
+		movement_vector = new Vector( 0, 0 );
+
+		if(gravity != null)
+		{
+			gravity_vector = gravity;
+		}
+		else
+		{
+			gravity_vector = new Vector( 0, 0 );
+		}
+
 		this.kinetic = kinetic;
 		this.collides = collides;
+
 	}
 
 	override function init()
 	{
-
+		entity.events.listen('collision', on_collision);
 	}
 
-	override function update()
+	override function update( dt:Float )
 	{
-
+		if(kinetic)
+		{
+			pos.add( Vector.Multiply( movement_vector, dt )  );
+			movement_vector.add( gravity_vector );
+			if(shape != null)
+			{
+				shape.x = pos.x;
+				shape.y = pos.y;
+			}
+		}
 	}
 
-	public function setShape( shape : Shape )
+	public function on_collision( data:CollisionData )
+	{
+		trace("Collision!");
+		pos.add( data.separation );
+		movement_vector.multiplyScalar(0);
+	}
+
+	public function set_shape( shape:Shape )
 	{
 		this.shape = shape;
 	}
 
-	public function setGravityVector( g : Vector )
+	public function set_gravity_vector( g:Vector )
 	{
 		gravity_vector = g;
 	}
 
-	public function setGravity( g : Bool )
+	public function set_gravity( g:Bool )
 	{
 		gravity = g;
 	}
--- src/Ruins.hx
+++ src/Ruins.hx
@@ -4,18 +4,65 @@
 *	once the minimum viable prototype is complete. 
 */
 import luxe.Input;
+import luxe.Sprite;
+import luxe.Vector;
+import luxe.Color;
+
+import luxe.collision.Collision;
+import luxe.collision.CollisionData;
+import luxe.collision.shapes.Shape;
+import luxe.collision.shapes.Circle;
+import luxe.collision.shapes.Polygon;
 
 class Ruins extends luxe.Game
 {
+	var collideables:Array<Sprite>; // Collection of sprites with physics
 	
-	override function init()
+	override function ready()
 	{
+		// TODO(louis): Set up collision system
+		collideables = new Array<Sprite>();
 
+		var playerSprite = new Sprite({
+			name: 'player',
+			pos: Luxe.screen.mid,
+			size: new Vector( 32, 32 ),
+			color: new Color().rgb(0xccddff)
+			});
+		var playerCol = new Physics( true, true, new Vector( 0, 50 ) );
+			playerCol.set_shape(Polygon.rectangle( 0, 0, 32, 32 ));
+		
+		playerSprite.add(playerCol);
+
+		collideables.push( playerSprite );
+
+		var groundSprite = new Sprite({
+			name: 'ground',
+			size: new Vector( Luxe.screen.w, 32 ),
+			pos: new Vector( Luxe.screen.w / 2, Luxe.screen.h - 16 ),
+			color: new Color().rgb(0xffddee)
+			});
 	}	
 
 	override function update( dt:Float )
 	{
-
+		for(isprite in collideables)
+		{
+			var iphysic = isprite.get('physics');
+			for(asprite in collideables)
+			{
+				var aphysic = asprite.get('physics');
+				var collisionTest = Collision.test( iphysic, aphysic ); 
+				if(collisionTest != null)
+				{
+					isprite.events.fire('collision', collisionTest);
+				}
+				else
+				{
+					trace("No collision!");
+				}
+			}
+		}
 	}
 
 	override function onkeyup( e:KeyEvent )
